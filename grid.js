5f8ede632311c183c95a98164038b9f2ae285c03
Fred Pelogia <fredpelogia@outlook.com>
2020-10-01 00:45:00 -0300

    added grid class... maybe it will be useful later

---
+++ grid.js
@@ -0,0 +1,25 @@
+class grid{
+	
+	constructor(x,y){
+		this.x = x;
+		this.y = y;
+
+		//maybe this class becomes useful later
+
+
+	}
+
+
+
+
+
+	obstacles(){
+		//think about it later
+	}
+
+
+
+
+
+
+}
